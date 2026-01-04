c52e5ec105a713b05c230571e4cdb715ef849a08
Joshua Fawcett <joshfawcettshs10@gmail.com>
2026-01-03 23:56:48 -0800

    Added debug logging to server

--- server/index.js
+++ server/index.js
@@ -14,6 +14,8 @@ const app = express();
 
 app.use((req, res, next) => {
 	logger.info("Incoming request", `${req.method} ${req.url}`);
+	logger.debug("request payload", JSON.stringify(req));
+	logger.debug("allowed", config.CLIENT_URL);
 	next();
 })
 
